5e8231bd0d910b560fa53b7cd889a64b8738f8f8
IFFranciscoME <if.francisco.me@gmail.com>
2021-02-08 15:20:50 -0600

    initial commit

---
+++ GD_salary_blank.py
@@ -0,0 +1,10 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- Project: Homework for class MSC1007A (Machine Learning) - M.Sc in Data Science - ITESO              -- #
+# -- File: GD_salary_blank.py - content for notebook                                                     -- #
+# -- Author: IFFranciscoME - if.francisco.me@gmail.com                                                   -- #
+# -- License: GPL-3.0 License                                                                            -- #
+# -- Repository: https://github.com/IFFranciscoME/ML_Gradient_Descent                                    -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
